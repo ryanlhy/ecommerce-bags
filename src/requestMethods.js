c94f21c56fa911f2eb89dccc2bc6963145258a1e
ryanlaw <ryan_law-rebound@hotmail.com>
2022-12-12 23:37:40 +0800

    change base url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://ecommerce-bags-backend.cyclic.app/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
