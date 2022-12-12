30b3bd118ea8d6225374306df758ea90249e0599
ryanlaw <ryan_law-rebound@hotmail.com>
2022-12-12 23:40:51 +0800

    changed name to bags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,7 @@ const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo>LAMA.</Logo>
+        <Logo>BAGS.</Logo>
         <Desc>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
@@ -136,7 +136,7 @@ const Footer = () => {
           <Phone style={{marginRight:"10px"}}/> +1 234 56 78
         </ContactItem>
         <ContactItem>
-          <MailOutline style={{marginRight:"10px"}} /> contact@lama.dev
+          <MailOutline style={{marginRight:"10px"}} /> contact@bags.dev
         </ContactItem>
         <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
       </Right>
